62e032c061b1da2c784740f008a4553314fac275
Denis Kudia <dkudja@gmail.com>
2025-10-10 17:11:25 -0700

    Dict. Added save/restore

--- cpp/dict_class.h
+++ cpp/dict_class.h
@@ -17,14 +17,17 @@
 #include <string>
 #include <cassert>
 #include <mutex>
+#include <atomic>
+#include <fstream>
+#include <utility>
 
 namespace wiki{
 
 template<typename K, typename V>
 class DictClass{
 public:
-    DictClass(){
-
+    DictClass(const std::string& name) : dict_name(name) {
+        dict_filename = name + ".csv";
     }
 
     /**
@@ -34,9 +37,36 @@ public:
      * @return true
      * @return false
      */
-    virtual const bool load(const std::string& filename){
+    virtual const int load(const std::string& filename = ""){
+        int count = 0;
+        save_load_active.store(true);
+        const std::lock_guard<std::mutex> lock(mtx);
 
-        return true;
+        std::ifstream inputFile((filename.empty() ? dict_filename : filename));
+        if(inputFile.is_open()){
+            std::string line;
+            while(std::getline(inputFile, line)){
+                const std::string item_id = line.substr(0, line.find(";"));
+                p_dict_exists[item_id] = item_id;
+                count++;
+            }
+
+            inputFile.close();
+        }
+
+        save_load_active.store(false);
+        return count;
+    }
+
+    template <typename Tuple, typename F, std::size_t... I>
+    void for_each_impl(Tuple&& t, F&& f, std::index_sequence<I...>) {
+        (f(std::get<I>(std::forward<Tuple>(t))), ...); // Fold expression (C++17)
+    }
+
+    template <typename Tuple, typename F>
+    void for_each(Tuple&& t, F&& f) {
+        for_each_impl(std::forward<Tuple>(t), std::forward<F>(f),
+                    std::make_index_sequence<std::tuple_size_v<std::decay_t<Tuple>>>{});
     }
 
     /**
@@ -46,9 +76,29 @@ public:
      * @return true
      * @return false
      */
-    virtual const bool save(const std::string& filename){
-        return true;
+    virtual const bool save(const std::string& filename = ""){
+        bool result = true;
+        save_load_active.store(true);
+
+        std::fstream outputFile((filename.empty() ? dict_filename : filename), std::ios::out | std::ios::app);
+        if(outputFile.is_open()){
+            outputFile.seekg(0, std::fstream::end);
+            for( auto it = p_dict.begin(); it != p_dict.end(); ++it ){
+                outputFile << it->first;
+                for_each(it->second, [&](const auto& val) {
+                    outputFile << ";" << val;
+                });
+                outputFile << std::endl;
+            }
+            outputFile.close();
+        }
+        else
+            result = false;
+
+        save_load_active.store(false);
+        return result;
     }
+
 
     /**
      * @brief
@@ -59,13 +109,27 @@ public:
     void put(const K& key, const V& val){
         assert((std::string(typeid(val).name()).find("tuple") != std::string::npos));
 
-        const std::lock_guard<std::mutex> lock(mtx);
+        if(save_load_active.load()) //do nothing if we load or save data
+            return;
 
+        //check if we have already had this key before
+        if( p_dict_exists.size() > 0 && p_dict_exists.end() != p_dict_exists.find(key)){
+            return;
+        }
+
+        const std::lock_guard<std::mutex> lock(mtx);
+        p_dict[key] = val;
     }
+
 
 private:
     std::mutex mtx;
     std::map<K,V> p_dict;
+    std::map<K,K> p_dict_exists;
+
+    std::string dict_name;
+    std::string dict_filename;
+    std::atomic_bool save_load_active = false;
 };
 
 }//namespace wiki
--- cpp/test_dict_class.cpp
+++ cpp/test_dict_class.cpp
@@ -13,18 +13,55 @@
 #include <string>
 #include <iostream>
 
+#include <fstream>
+#include <memory>
+#include <random>
+#include <tuple>
+
 #include "dict_class.h"
 
-using dict_2 = std::tuple<std::string, std::string>;
+using dict_key = std::string;
+using dict_val = std::tuple<std::string, std::string>;
 
 int main (int argc, char* argv[])
 {
-    const std::type_info& r1 = typeid(dict_2);
-    std::cout << " Is tuple: " << (std::string(typeid(dict_2).name()).find("tuple") != std::string::npos) << "" << typeid(dict_2).name() << std::endl;
-    std::cout << " " << typeid(std::string).name() << std::endl;
+    //const std::type_info& r1 = typeid(dict_2);
+    //std::cout << " Is tuple: " << (std::string(typeid(dict_2).name()).find("tuple") != std::string::npos) << " Name: " << typeid(dict_2).name() << std::endl;
+    //std::cout << " " << typeid(std::string).name() << std::endl;
+
+    std::mt19937_64 rng(std::chrono::high_resolution_clock::now().time_since_epoch().count());
+    std::uniform_int_distribution<unsigned int> dist(1, 10000000);
 
 
-    //wiki::DictClass<dict_2> dict_2;
+    std::shared_ptr<wiki::DictClass<dict_key, dict_val>> dict = std::make_shared<wiki::DictClass<dict_key, dict_val>>("P31");
+    auto l_rec = dict->load();
+    std::cout << "Loaded records: " << l_rec << std::endl;
+
+    for(int i = 0; i < 100; i++){
+        unsigned int randomNumber = dist(rng);
+        const std::string item = "Q" + std::to_string(randomNumber);
+        const auto val = std::make_tuple(std::string("item"), std::string("place of birth"));
+        dict->put(item, val);
+    }
+
+    dict->save();
+    dict.reset();
+
+
+/*
+    std::ifstream inputFile("P31.csv");
+    if(inputFile.is_open()){
+        std::string line;
+        int i_line = 0;
+        while(std::getline(inputFile, line)){
+            const std::string item_id = line.substr(0, line.find(";"));
+            std::cout << i_line << " " << item_id << std::endl;
+            i_line++;
+        }
+
+    }
+    inputFile.close();
+*/
 
     exit(EXIT_SUCCESS);
 }
